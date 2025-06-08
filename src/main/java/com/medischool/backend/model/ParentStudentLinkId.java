50b7d9bdb258dd439ed4c17384ec3a030dd8b6ee
Minh Nguyen <177280815+ngxtm@users.noreply.github.com>
2025-06-08 12:32:49 +0700

    Add parent
    model

---
+++ src/main/java/com/medischool/backend/model/ParentStudentLinkId.java
@@ -0,0 +1,16 @@
+package com.medischool.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ParentStudentLinkId implements Serializable {
+    private UUID parentId;
+    private Integer studentId;
+}
