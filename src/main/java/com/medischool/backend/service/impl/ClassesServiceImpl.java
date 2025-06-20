565b55767581f35266ea5c46ee0cfc5a96342450
Minh Nguyen <177280815+ngxtm@users.noreply.github.com>
2025-06-20 16:02:38 +0700

    Adds class management functionality
    Adds functionality to retrieve all classes.
    It implements the controller, service, and repository layers.

---
+++ src/main/java/com/medischool/backend/service/impl/ClassesServiceImpl.java
@@ -0,0 +1,21 @@
+package com.medischool.backend.service.impl;
+
+import com.medischool.backend.model.ParentStudent.Classes;
+import com.medischool.backend.repository.ClassesRepository;
+import com.medischool.backend.service.ClassesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ClassesServiceImpl implements ClassesService {
+
+    private final ClassesRepository classesRepository;
+
+    @Override
+    public List<Classes> findAll() {
+        return classesRepository.findAll();
+    }
+}
