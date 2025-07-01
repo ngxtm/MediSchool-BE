edbf3d565d019a14fc13a3588f7ee19b615491d7
Minh Nguyen <177280815+ngxtm@users.noreply.github.com>
2025-07-01 20:12:05 +0700

    Add health event model

--- src/main/java/com/medischool/backend/model/healthevent/HealthEvent.java
+++ src/main/java/com/medischool/backend/model/healthevent/HealthEvent.java
@@ -1,4 +1,39 @@
 package com.medischool.backend.model.healthevent;
 
+import com.medischool.backend.model.enums.Extent;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "health_event")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class HealthEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "event_id")
+    private Long id;
+
+    @Column(name = "student_id")
+    private Integer studentId;
+
+    private String problem;
+    private String description;
+    private String solution;
+
+    @Column(name = "event_time")
+    private OffsetDateTime eventTime;
+
+    @Column(name = "record_by")
+    private UUID recordBy;
+
+    private Extent extent;
 }
