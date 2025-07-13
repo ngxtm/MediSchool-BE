f6d765f9fae944940261b1f9cc88e582f221f745
Minh Nguyen <177280815+ngxtm@users.noreply.github.com>
2025-07-13 20:02:14 +0700

    Fix v0.2

--- src/main/java/com/medischool/backend/controller/LoginHistoryController.java
+++ src/main/java/com/medischool/backend/controller/LoginHistoryController.java
@@ -6,11 +6,7 @@ import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.medischool.backend.dto.LoginHistoryDTO;
 import com.medischool.backend.model.LoginHistory.LoginStatus;
@@ -19,6 +15,12 @@ import com.medischool.backend.service.LoginHistoryService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "http://localhost:8080",
+        "https://medischool-be.onrender.com",
+        "https://medischool.vercel.app"
+})
 @RequestMapping("/api/admin/login-history")
 @RequiredArgsConstructor
 public class LoginHistoryController {
